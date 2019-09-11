6e433d566d4a581acb499bee7744513d4afa4c9a
Tim <tim.tannert@tu-dortmund.de>
2019-09-11 15:50:48 +0200

    Space-Measure vers 1

--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,54 @@ extern crate rmp_serde as rmps;
 use stats_alloc::{Region, StatsAlloc, INSTRUMENTED_SYSTEM};
 
 use std::alloc::System;
-use std::fs::read_dir;
 use std::fmt::Debug;
-use std::fs::File;
-use std::io::BufReader;
-
-use stree::u40::stat::STree;
-use stree::internal::PredecessorSetStatic;
+use std::fs::{OpenOptions, File, read_dir};
+use std::io::{BufWriter, BufReader, Write};
+use ma_titan::default::immutable::STree;
+use ma_titan::internal::PredecessorSetStatic;
 
 use uint::u40;
 use uint::Typable;
 
 use serde::Deserialize;
+use serde::de::DeserializeOwned;
 use rmps::Deserializer;
 #[global_allocator]
 static GLOBAL: &StatsAlloc<System> = &INSTRUMENTED_SYSTEM;
 
 fn main() {
     
-    measure::<u40,STree>();
+    measure::<u40,STree<u40>>();
     // Used here to ensure that the value is not
     // dropped before we check the statistics
     //::std::mem::size_of_val(&x);
 }
 
-fn measure<E: 'static + Typable + Copy + Debug + From<u64>, T: PredecessorSetStatic<E>>() {
-    for dir in read_dir(format!("../testdata/{}/", E::TYPE)).unwrap() {
+fn measure<E: 'static + Typable + Copy + Debug + From<u64> + DeserializeOwned, T: PredecessorSetStatic<E>>() {
+    let mut result = BufWriter::new(OpenOptions::new()
+        .read(true)
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("stats.txt").unwrap());
+
+    for dir in read_dir(format!("../ma_titan/testdata/{}/", E::TYPE)).unwrap() {
         let dir = dir.unwrap();
         let path = dir.path();
-        let p = format!("{:?}", path);
 
         let buf = BufReader::new(File::open(path).unwrap());
         
         
         let mut values = Deserializer::new(buf);
-        let values: Vec<u64> = Deserialize::deserialize(&mut values).unwrap();
-        let values = values.into_iter().map(|v| E::from(v)).collect::<Vec<E>>();
+        let values: Vec<E> = Deserialize::deserialize(&mut values).unwrap();
+        let len = values.len();
 
         let reg = Region::new(&GLOBAL);
-        let x = T::new(values);
-        println!("{}: {:#?}", p,reg.change());
+        let _ = T::new(values);
+        let change = reg.change();
+
+        let build_size = change.bytes_allocated + change.bytes_allocated.max(0);
+        let final_size = change.bytes_allocated as isize + change.bytes_reallocated + change.bytes_deallocated as isize;
+        writeln!(result, "RESULT algo={} method=new size={} build_size_bytes={} size_bytes={}",T::TYPE,len,build_size,final_size ).unwrap(); 
     }
 }
