36379cd8d1ffceb4afadb2ab5577f7e9fe83b3ca
Tim <tim.tannert@tu-dortmund.de>
2019-09-26 09:37:53 +0200

    Normalverteilung eingefügt

--- src/main.rs
+++ src/main.rs
@@ -31,13 +31,13 @@ fn main() {
         .write(true)
         .truncate(true)
         .create(true)
-        .open("stats.txt").unwrap());
+        .open("stats_uniform.txt").unwrap());
 
     // Messen für STree<u40>
-    measure::<u40,STree<u40>>(&mut result);
+    measure_uniform::<u40,STree<u40>>(&mut result);
 
     // Messen für BinarySearch<u40> (Baseline)
-    measure::<u40,BinarySearch<u40>>(&mut result);
+    measure_uniform::<u40,BinarySearch<u40>>(&mut result);
 
     
     // Used here to ensure that the value is not
@@ -49,9 +49,63 @@ fn main() {
 
 /// Diese Methode dient der Hauptspeichermessung der new()-Methode verschiedener zu untersuchender Datenstrukturen E
 /// mit elementen E = {u40,u48,u64} .
-fn measure<E: 'static + Typable + Copy + Debug + From<u64> + DeserializeOwned, T: PredecessorSetStatic<E>>(result: &mut BufWriter<File>) {
+fn measure_uniform<E: 'static + Typable + Copy + Debug + From<u64> + DeserializeOwned, T: PredecessorSetStatic<E>>(result: &mut BufWriter<File>) {
 
-    for dir in read_dir(format!("../ma_titan/testdata/{}/", E::TYPE)).unwrap() {
+    for dir in read_dir(format!("../ma_titan/testdata/uniform/{}/", E::TYPE)).unwrap() {
+        let dir = dir.unwrap();
+        let path = dir.path();
+        println!("{:?}",path);
+
+        let buf = BufReader::new(File::open(path).unwrap());
+        
+        
+        let mut values = Deserializer::new(buf);
+
+        let mut reg = Region::new(&GLOBAL);
+        let values: Vec<E> = Deserialize::deserialize(&mut values).unwrap();
+        let len = values.len();
+
+ 
+        let x = T::new(values);
+        let change = reg.change_and_reset();
+
+        // Das Ergebnis wird in die stats.txt geschrieben, die von SQLPlots analysiert und geplottet werden kann
+        writeln!(result, "RESULT data_structure={} method=new size={} build_size_bytes={} size_bytes={}",T::TYPE,len,change.bytes_max_used,change.bytes_current_used + std::mem::size_of_val(&x) ).unwrap(); 
+    }
+}
+
+/// Diese Methode dient der Hauptspeichermessung der new()-Methode verschiedener zu untersuchender Datenstrukturen E
+/// mit elementen E = {u40,u48,u64} .
+fn measure_normal_viertel<E: 'static + Typable + Copy + Debug + From<u64> + DeserializeOwned, T: PredecessorSetStatic<E>>(result: &mut BufWriter<File>) {
+
+    for dir in read_dir(format!("../ma_titan/testdata/normal/bereich_viertel/{}/", E::TYPE)).unwrap() {
+        let dir = dir.unwrap();
+        let path = dir.path();
+        println!("{:?}",path);
+
+        let buf = BufReader::new(File::open(path).unwrap());
+        
+        
+        let mut values = Deserializer::new(buf);
+
+        let mut reg = Region::new(&GLOBAL);
+        let values: Vec<E> = Deserialize::deserialize(&mut values).unwrap();
+        let len = values.len();
+
+ 
+        let x = T::new(values);
+        let change = reg.change_and_reset();
+
+        // Das Ergebnis wird in die stats.txt geschrieben, die von SQLPlots analysiert und geplottet werden kann
+        writeln!(result, "RESULT data_structure={} method=new size={} build_size_bytes={} size_bytes={}",T::TYPE,len,change.bytes_max_used,change.bytes_current_used + std::mem::size_of_val(&x) ).unwrap(); 
+    }
+}
+
+/// Diese Methode dient der Hauptspeichermessung der new()-Methode verschiedener zu untersuchender Datenstrukturen E
+/// mit elementen E = {u40,u48,u64} .
+fn measure_normal_komplett<E: 'static + Typable + Copy + Debug + From<u64> + DeserializeOwned, T: PredecessorSetStatic<E>>(result: &mut BufWriter<File>) {
+
+    for dir in read_dir(format!("../ma_titan/testdata/normal/bereich_komplett/{}/", E::TYPE)).unwrap() {
         let dir = dir.unwrap();
         let path = dir.path();
         println!("{:?}",path);
