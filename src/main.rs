ffebdda6223baeca393baba128fe67c84f2b742b
Tim <tim.tannert@tu-dortmund.de>
2019-09-26 12:47:12 +0200

    Unterer Hashfunktionsbereich

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,8 @@ fn main() {
         .write(true)
         .truncate(true)
         .create(true)
-        .open("stats_hashmap_size.txt").unwrap());
-    //    .open("stats_mphf_size.txt").unwrap());
+    //    .open("stats_hashmap_size.txt").unwrap());
+        .open("stats_mphf_size.txt").unwrap());
     //measure::<u40,STree<u40>>(&mut result);
 
     //measure::<u40,BinarySearch>(&mut result);
@@ -37,21 +37,19 @@ fn main() {
     // Used here to ensure that the value is not
     // dropped before we check the statistics
 
-    for i in 1..u16::max_value() {
-        if i%1000==1 {
-            //let keys = (0..i).collect();
-            let mut reg = Region::new(&GLOBAL);
-            //let h = Mphf::new_parallel(2.0, &keys, None);
-            let h: HashMap<u16,usize> = HashMap::with_capacity(i as usize);
-            let change = reg.change_and_reset();
-           
-            let build_size = change.bytes_max_used + std::mem::size_of_val(&h);
-            let final_size = change.bytes_current_used + std::mem::size_of_val(&h) - (i as u64*40/8) as usize; // Die gespeicherten Elemente abziehen
-            
-            //writeln!(result, "RESULT data_structure=Mphf-u16,usize- method=new size={} build_size_bytes={} size_bytes={}",i,build_size,final_size ).unwrap(); 
-            writeln!(result, "RESULT data_structure=HashMap-u16,usize- method=new size={} build_size_bytes={} size_bytes={}",i,build_size,final_size ).unwrap(); 
+    for i in 1..1024 {
+        let keys = (0..i).collect();
+        let mut reg = Region::new(&GLOBAL);
+        let h = Mphf::new_parallel(2.0, &keys, None);
+        //let h: HashMap<u16,usize> = HashMap::with_capacity(i as usize);
+        let change = reg.change_and_reset();
         
-        }
+        let build_size = change.bytes_max_used + std::mem::size_of_val(&h);
+        let final_size = change.bytes_current_used + std::mem::size_of_val(&h); // Die gespeicherten Elemente abziehen
+        
+        writeln!(result, "RESULT data_structure=Mphf-u16,usize- method=new size={} build_size_bytes={} size_bytes={}",i,build_size,final_size ).unwrap(); 
+        //writeln!(result, "RESULT data_structure=HashMap-u16,usize- method=new size={} build_size_bytes={} size_bytes={}",i,build_size,final_size ).unwrap(); 
+    
     }
 }
 
