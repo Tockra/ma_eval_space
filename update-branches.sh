53a6e8142e9c840966c417c90233c40271920883
Tim <tim.tannert@tu-dortmund.de>
2019-10-21 22:00:16 +0200

    update-branches added

---
+++ update-branches.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+git checkout master &&
+git pull &&
+git checkout space_efficient &&
+git merge --no-edit master &&
+git push &&
+git checkout fnv_hash &&
+git merge --no-edit master &&
+git push &&
+git checkout rustc_hash_hash &&
+git merge --no-edit master &&
+git push &&
+git checkout hash_brown_hash &&
+git merge --no-edit master &&
+git push &&
+git checkout space_effcient_128 &&
+git merge --no-edit master &&
+git push 
+git checkout space_effcient_max &&
+git merge --no-edit master &&
+git push 
