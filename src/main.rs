bc9a1b681878ee26132ed757e57e4ffbd0bf4d3e
Tim <tim.tannert@tu-dortmund.de>
2019-10-22 00:09:01 +0200

    brown hinzugefügt hashmap ausgewertet

--- src/main.rs
+++ src/main.rs
@@ -31,19 +31,32 @@ fn main() {
             .truncate(true)
             .create(true)
             .open("stats_hashmap_size.txt").unwrap());
+            let item = 0_u8;
+                for i in 1..u16::max_value() {
+                    let i = i as usize;
+                    if i % 1 == 0 {
+                        let keys: Vec<u16> = (0..i as u16).collect();
+                        {
+                        let reg = Region::new(&GLOBAL);
+                
+                        let mut h = hashbrown::HashMap::with_capacity(i);
+                        for k in keys.into_iter() {
+                            h.insert(k, item);
+                        }
 
-                for i in 1..1024 {
-                    let mut reg = Region::new(&GLOBAL);
-                    let change;
+  
+                        let change = reg.change();
+        
+                        
+                        let build_size = change.bytes_max_used;
+                        
+                        let final_size = change.bytes_current_used + std::mem::size_of_val(&h); // Die gespeicherten Elemente abziehen
 
-                    let h: HashMap<u16,usize> = HashMap::with_capacity(i as usize);
-                    change = reg.change_and_reset();
-                    
-                    let build_size = change.bytes_max_used;
-                    let final_size = change.bytes_current_used; // Die gespeicherten Elemente abziehen
-
-             
-                    writeln!(result, "RESULT data_structure=HashMap-u16,usize- method=new size={} build_size_bytes={} size_bytes={}",i,build_size,final_size).unwrap(); 
+                        writeln!(result, "RESULT data_structure=Brown_Hash-u16,usize- method=new size={} build_size_bytes={} size_bytes={}",i,build_size,((final_size as f64)/(i as f64)) * 8. ).unwrap();
+                        // 32 da ein extra Array vorhanden ist das 16 Byte braucht (8 Zeiger, 8 Len) + Box (8). Außerdem muss nun Object auch in einer Box leigen + 8
+                        writeln!(result, "RESULT data_structure=Vektor method=new size={} build_size_bytes=0 size_bytes={}",i,(32.+(i as f64) * 2. + (i as f64) + std::mem::size_of_val(&item) as f64)/(i as f64) *8. ).unwrap(); 
+                    }
+                    }
                 }
 
         },
@@ -64,7 +77,7 @@ fn main() {
                 for i in 1..2048 {
                     let i = i as usize;
                     if i % 1 == 0 {
-                        let keys = (0..i).collect();
+                        let keys = (0..i as u16).collect();
                         {
                         let reg = Region::new(&GLOBAL);
                 
